47f9a31c83c33a380db4124eb3e5be4bfedb1ba7
Boris MBARGA <bmbarga@e2r11p1.42.fr>
2016-10-18 19:52:58 +0200

    je suis con

--- rt_v1/srcs/raytracer/rt_get_color.c
+++ rt_v1/srcs/raytracer/rt_get_color.c
@@ -6,7 +6,7 @@
 /*   By: bmbarga <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/10/17 20:06:39 by bmbarga           #+#    #+#             */
-/*   Updated: 2016/10/18 19:06:10 by bmbarga          ###   ########.fr       */
+/*   Updated: 2016/10/18 19:52:17 by bmbarga          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,16 +20,45 @@
 #include "material.h"
 #include "rt_color.h"
 
-/*
-static void			rt_light_effect(t_rt *rt, t_ray *ray,
-								t_light *light, t_obj *o, t_pos p)
+static t_base	get_base(t_pos p, t_pos l, t_pos n, t_pos cam)
 {
-	t_s_color	s_col;
-	t_ray		r;
-	t_pos		n;
-	t_pos		l;
-	t_base		base;
+	t_base	b;
+	
+	b.o = p;
+	b.i = l;
+	b.j = n;
+	b.k = cam;
+	return (b);
 }
+
+static void		get_light(t_rt *rt, t_s_color *s_col, t_light *light,
+						t_obj *o, t_pos p, t_ray *ray)
+{
+	t_ray		r;
+	t_base		b;
+
+	b.o = (o->normal) ? o->normal(o, ray->rd, p) : get_pos(0, 0, 0);
+	b.i = pos_vector(p, light->sp.o);
+	r.rd = pos_normalize(b.i);
+	r.pos = light->sp.o;
+	r.ro = p;
+	if (enlightened(rt, o, r))
+	{
+		if (rt->diffuse)
+			*s_col = add_s_color(*s_col,
+					diffuse_light(light, o, b.i, b.o));
+		if (rt->specular)
+			*s_col = add_s_color(*s_col,
+					specular_light(light, o, get_base(p, b.i, b.o, rt->camera->sp.o)));
+	}
+	else
+		*s_col = sub_s_color(*s_col,
+				get_s_color(s_col->r / SHADOW_COEF,
+					s_col->g / SHADOW_COEF, s_col->b / SHADOW_COEF));
+}
+
+/*
+** base b is used to store normal and light direction
 */
 
 void			rt_get_color(t_ray *ray, t_obj *o, t_rt *rt, t_pos p)
@@ -37,10 +66,6 @@ void			rt_get_color(t_ray *ray, t_obj *o, t_rt *rt, t_pos p)
 	t_list		*list;
 	t_light		*light;
 	t_s_color	s_col;
-	t_ray		r;
-	t_pos		n;
-	t_pos		l;
-	t_base		base;
 
 
 	if (!rt || !ray || !o)
@@ -53,31 +78,8 @@ void			rt_get_color(t_ray *ray, t_obj *o, t_rt *rt, t_pos p)
 		light = (t_light*)list->content;
 		if (light)
 		{
-			l = pos_vector(p, light->sp.o);
-			r.rd = pos_normalize(l);
-			r.pos = light->sp.o;
-			r.ro = p;
 			if (o->normal)
-			{
-				if (enlightened(rt, o, r))
-				{
-					n = o->normal(o, ray->rd, p);
-					if (rt->diffuse)
-						s_col = add_s_color(s_col,
-								diffuse_light(light, o, l, n));
-					if (rt->specular)
-					{
-						base.o = p;
-						base.i = l;
-						base.j = n;
-						base.k = rt->camera->sp.o;
-						s_col = add_s_color(s_col, specular_light(light, o, base));
-					}
-				}
-				else
-					s_col = sub_s_color(s_col,
-							get_s_color(s_col.r / SHADOW_COEF, s_col.g / SHADOW_COEF, s_col.b / SHADOW_COEF));
-			}
+				get_light(rt, &s_col, light, o, p, ray);
 		}
 		list = list->next;
 	}
